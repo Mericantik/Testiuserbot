9dea3b703bb735e3bc659f5062af7276469854b3
Mericantik <146951316+Mericantik@users.noreply.github.com>
2024-01-04 12:14:22 +0700

    Update mute.py

--- modules/mute.py
+++ modules/mute.py
@@ -57,7 +57,7 @@ async def startmute(event):
     elif event.is_private:
         userid = event.chat_id
     else:
-        return await xx.eor("`Balas ke pengguna atau tambahkan userid mereka.`", time=5)
+        return await xx.eor("`Balas ke pengguna atau tambahkan kata terlarabg.`", time=5)
     chat = await event.get_chat()
     if "admin_rights" in vars(chat) and vars(chat)["admin_rights"] is not None:
         if not chat.admin_rights.delete_messages:
@@ -86,7 +86,7 @@ async def endmute(event):
     elif event.is_private:
         userid = event.chat_id
     else:
-        return await xx.eor("`Balas ke pengguna atau tambahkan userid mereka.`", time=5)
+        return await xx.eor("`Balas ke pengguna atau tambahkan kata terlarang.`", time=5)
     if not is_muted(event.chat_id, userid):
         return await xx.eor("`Pengguna ini tidak dibisukan dalam obrolan ini.`", time=3)
     unmute(event.chat_id, userid)
